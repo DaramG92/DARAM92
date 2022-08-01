470ca58d60b0925a3d90dc873cf42320eb0c04b4
DaramG92 <108664430+DaramG92@users.noreply.github.com>
2022-08-01 22:08:11 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -12,8 +12,3 @@ $('#wrap').fullpage({
 //     navigation:true
 //     // ㄴ 오른쪽에 페이지 올라갔다 내려갔다 점3개 생김
 // });
-
-$('#wrap .section:nth-child(2) .slide:nth-child(1)').on('click',function(){
-   
-    window.open('about:blank').location.href="https://google.com";
-});
