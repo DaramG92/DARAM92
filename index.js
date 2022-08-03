56e4457ecdee53511a38e635b1da554723e6f06a
DaramG92 <108664430+DaramG92@users.noreply.github.com>
2022-08-03 21:29:11 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ $('#wrap').fullpage({
     navigation:true
 });
 
-// new fullpage('#wrap',
-//     {
-//     autoScrolling:true ,
-//     anchors:['1page' , '2page' , '3page'],
-//     navigation:true
-//     // ㄴ 오른쪽에 페이지 올라갔다 내려갔다 점3개 생김
-// });
+$('#wrap .section:nth-child(3)').on('click',function(){
+   
+    window.open('about:blank').location.href="https://daramg92.github.io/DD92/";
+});
